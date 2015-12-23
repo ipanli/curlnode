43b5226fef3a3f16fb80b7e700253e7d0723be7a
zanjs <root@zanjs.com>
2015-12-23 10:16:39 +0800

    Wed Dec 23 2015 10:16:39 GMT+0800 (中国标准时间) By gitpush

--- index.js
+++ index.js
@@ -13,24 +13,74 @@ var nsq = require('nsqjs');
 
 
 
-var reader = new nsq.Reader('topic', 'test', {
-  lookupdHTTPAddresses: '120.24.210.90:4161'
-});
-
-reader.connect();
-
-reader.on('message', function (msg) {
-  console.log('Received message [%s]: %s', msg.id, msg.body.toString());
-  msg.finish();
-});
-
-
-request.post({
-		url:'http://120.24.210.90:4151/put?topic=test', 
-		form: {topic:'test'}
-		}, function(err,httpResponse,body){ 
-			console.info(body);
+request({
+    method: 'post',
+    preambleCRLF: true,
+    postambleCRLF: true,
+    uri: 'http://120.24.210.90:4151/put?topic=test',
+    multipart: [
+      {       
+        body: 'I am nodejs request messages'
+      }      
+    ]
+  },
+  function (error, response, body) {
+    if (error) {
+      return console.error('upload failed:', error);
+    }
+    console.log('Upload successful!  Server responded with:', body);
 })
+
+
+
+
+// var postData = querystring.stringify({
+//   'msg' : 'Hello World!'
+// });
+
+// var options = {
+//   hostname: '120.24.210.90',
+//   port: 4151,
+//   path: '/put?topic=test',
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/x-www-form-urlencoded',
+//     'Content-Length': postData.length
+//   }
+// };
+
+// var req = http.request(options, function(res) {
+//   console.log('STATUS: ' + res.statusCode);
+//   console.log('HEADERS: ' + JSON.stringify(res.headers));
+//   res.setEncoding('utf8');
+//   res.on('data', function (chunk) {
+//     console.log('BODY: ' + chunk);
+//   });
+//   res.on('end', function() {
+//     console.log('No more data in response.')
+//   })
+// });
+
+// req.on('error', function(e) {
+//   console.log('problem with request: ' + e.message);
+// });
+
+// // write data to request body
+// req.write(postData);
+// req.end();
+
+
+// var reader = new nsq.Reader('topic', 'test', {
+//   lookupdHTTPAddresses: '120.24.210.90:4161'
+// });
+
+// reader.connect();
+
+// reader.on('message', function (msg) {
+//   console.log('Received message [%s]: %s', msg.id, msg.body.toString());
+//   msg.finish();
+// });
+
 
 
 // app.use(function *(next){
